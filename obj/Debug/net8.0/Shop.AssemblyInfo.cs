fe308c0009e0204d5f9fbf978b43e2670b233a41
HasnZidan <Zedanh3@gmail.com>
2024-12-10 18:11:18 +0200

    .

--- obj/Debug/net8.0/Shop.AssemblyInfo.cs
+++ obj/Debug/net8.0/Shop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8be61d4225377de679cb1d2611afa4b5e76c3af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
