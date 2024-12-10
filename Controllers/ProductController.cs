b8be61d4225377de679cb1d2611afa4b5e76c3af
HasnZidan <Zedanh3@gmail.com>
2024-12-10 18:08:23 +0200

    Added Products , PoductsBl Models ,Enhanced UI: Added Products page, improved navigation, updated styling

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Products()
+        {
+            ProductBL product = new ProductBL();
+            List<Product> ProductListModel = product.GetProducts();
+            return View("Products",ProductListModel);
+        }
+        public IActionResult ProductDetails(int id)
+        {
+            ProductBL productBl = new ProductBL();
+            Product ProductModel = productBl.GetProductsById(id);
+            return View("ProductDetails",ProductModel);
+        }
+    }
+}
---
+++ Models/ProductBL.cs
@@ -0,0 +1,24 @@
+﻿namespace Shop.Models
+{
+    public class ProductBL
+    {
+        List<Product> products;
+
+        public ProductBL()
+        {
+            products = new List<Product>();
+            products.Add(new Product() { Id = 1 , Name = "Laptop", Description="empty",Price=20000, ImageURL= "Laptop.jpg" });
+            products.Add(new Product() { Id = 2 , Name = "taplet", Description="empty",Price=11000, ImageURL= "Taplet.jpg" });
+            products.Add(new Product() { Id = 3 , Name = "mouse", Description="empty",Price=200, ImageURL= "Mouse.jpg" });
+        }
+        public List<Product> GetProducts()
+        {
+            return products;
+        }
+        public Product GetProductsById(int id)
+        {
+            return products.FirstOrDefault(s => s.Id == id);
+        }
+
+    }
+}
